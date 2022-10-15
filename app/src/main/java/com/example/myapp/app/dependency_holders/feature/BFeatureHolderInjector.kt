3143b5a9939a9473552aa90fba48a37725325b1f
Serge Bashkatov <spinking@mail.ru>
2022-10-16 00:14:52 +0400

    refactored B and C injection

--- app/src/main/java/com/example/myapp/app/dependency_holders/feature/BFeatureHolderInjector.kt
+++ app/src/main/java/com/example/myapp/app/dependency_holders/feature/BFeatureHolderInjector.kt
@@ -1,10 +1,10 @@
 package com.example.myapp.app.dependency_holders.feature
 
+import com.example.a_feature_impl.di.AFeatureApi
+import com.example.a_feature_impl.di.AFeatureComponentHolder
 import com.example.modulinjection.BaseDependencyHolder
 import com.example.modulinjection.BaseFeatureDependencies
 import com.example.modulinjection.DependencyHolder1
-import com.example.myapp.di.AppComponentHolder
-import com.example.myapp.di.AppFeatureApi
 import com.example.b_feature_impl.di.BFeatureComponentHolder
 import com.example.b_feature_impl.di.BFeatureDependencies
 
@@ -13,11 +13,10 @@ object BFeatureHolderInjector {
         BFeatureComponentHolder.dependencyProvider = {
             class BFeatureComponentDependencyHolder(
                 override val block: (
-                    BaseDependencyHolder<BFeatureDependencies>, api1:
-                AppFeatureApi) -> BFeatureDependencies
-            ) : DependencyHolder1<AppFeatureApi, BFeatureDependencies>(api1 = AppComponentHolder.get())
+                    BaseDependencyHolder<BFeatureDependencies>, api1: AFeatureApi) -> BFeatureDependencies
+            ) : DependencyHolder1<AFeatureApi, BFeatureDependencies>(api1 = AFeatureComponentHolder.get())
 
-            BFeatureComponentDependencyHolder { dependencyHolder, appApi ->
+            BFeatureComponentDependencyHolder { dependencyHolder, aFeatureApi ->
                 object : BFeatureDependencies {
                     override val dependencyHolder: BaseDependencyHolder<out BaseFeatureDependencies> = dependencyHolder
                 }
--- app/src/main/java/com/example/myapp/app/dependency_holders/feature/CFeatureHolderInjector.kt
+++ app/src/main/java/com/example/myapp/app/dependency_holders/feature/CFeatureHolderInjector.kt
@@ -1,10 +1,10 @@
 package com.example.myapp.app.dependency_holders.feature
 
+import com.example.b_feature_impl.di.BFeatureApi
+import com.example.b_feature_impl.di.BFeatureComponentHolder
 import com.example.modulinjection.BaseDependencyHolder
 import com.example.modulinjection.BaseFeatureDependencies
 import com.example.modulinjection.DependencyHolder1
-import com.example.myapp.di.AppComponentHolder
-import com.example.myapp.di.AppFeatureApi
 import com.example.c_feature_impl.di.CFeatureComponentHolder
 import com.example.c_feature_impl.di.CFeatureDependencies
 
@@ -12,10 +12,10 @@ object CFeatureHolderInjector {
     fun cFeatureInjection(){
         CFeatureComponentHolder.dependencyProvider = {
             class CFeatureComponentDependencyHolder(
-                override val block: (BaseDependencyHolder<CFeatureDependencies>, api1: AppFeatureApi) -> CFeatureDependencies
-            ) : DependencyHolder1<AppFeatureApi, CFeatureDependencies>(api1 = AppComponentHolder.get())
+                override val block: (BaseDependencyHolder<CFeatureDependencies>, api1: BFeatureApi) -> CFeatureDependencies
+            ) : DependencyHolder1<BFeatureApi, CFeatureDependencies>(api1 = BFeatureComponentHolder.get())
 
-            CFeatureComponentDependencyHolder { dependencyHolder, appApi ->
+            CFeatureComponentDependencyHolder { dependencyHolder, bFeatureApi ->
                 object : CFeatureDependencies {
                     override val dependencyHolder: BaseDependencyHolder<out BaseFeatureDependencies> = dependencyHolder
                 }
